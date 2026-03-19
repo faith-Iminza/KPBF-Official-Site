71e8bf22414a3bace511345cee7f9d251c660708
faith-Iminza <52449998+faith-Iminza@users.noreply.github.com>
2026-03-19 09:29:56 +0300

    updates js file

--- js/main.js
+++ js/main.js
@@ -74,6 +74,22 @@
         ]
     });
 
-    
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+
+    card.addEventListener("mouseenter", () => {
+
+        cards.forEach(c => c.classList.remove("active"));
+
+        card.classList.add("active");
+
+    });
+
+});
+        
 })(jQuery);
+
+
+
 
